8187229bdc7224412c97850be5e85bd2e0cf48d0
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-06 13:13:41 +0530

    db connectivity : CRUD operations :select

--- day-3/java-database-connectivity/src/main/java/Main.java
+++ day-3/java-database-connectivity/src/main/java/Main.java
@@ -1,27 +1,25 @@
 import db.Connectivity;
 import service.DbService;
 
-import java.sql.Date;
 import java.sql.SQLException;
-import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
         try {
             Connectivity connectivity = new Connectivity();
-
             DbService service = new DbService(connectivity.getConnection());
-            int cnt = service.create(
-                    4,
-                    "abc",
-                    Date.valueOf(LocalDate.now()),
-                    true
-            );
-            if (cnt > 0) {
-                System.out.println("Employee Created Successfully");
-            }
-            service.find();
+//            int cnt = service.create(
+//                    2,
+//                    "pqr",
+//                    Date.valueOf(LocalDate.now()),
+//                    true
+//            );
+//            if(cnt > 0) {
+//                System.out.println("Employee Created Successfully");
+//            }
+//            service.find();
 
+            service.findEmployeeByName("abc");
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
