067af9064903519f9d96560ec0b00ad566f0ccda
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-06 15:09:38 +0530

    db connectivity : CRUD operations :txn Management completed

--- day-3/java-database-connectivity/src/main/java/db/Connectivity.java
+++ day-3/java-database-connectivity/src/main/java/db/Connectivity.java
@@ -2,25 +2,26 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class Connectivity {
 
     private final Connection connection;
 
     public Connectivity() throws ClassNotFoundException, SQLException {
-        // it loads the driver into memory
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver"); // -> jdbc
         // mysql -u root -p
         // here you are giving username and password for connectivity
-        connection = DriverManager.getConnection(
+        connection = DriverManager.getConnection( // -> jdbc
                 "jdbc:mysql://localhost:3306/java_training_db",
                 "root",
                 "password"
-        );
+        );// it loads the driver into memory
+
+        connection.setAutoCommit(false);
         System.out.println("---- Connected to MySQL Successfully ----");
     }
 
     public Connection getConnection() {
-
         return connection;
     }
 }
--- day-3/java-database-connectivity/src/main/java/service/DbService.java
+++ day-3/java-database-connectivity/src/main/java/service/DbService.java
@@ -4,12 +4,10 @@ import java.sql.*;
 
 public class DbService {
     private final Connection connection;
-
     public DbService(Connection connection) {
         this.connection = connection;
     }
-
-    // insert query
+    // insert query - saving data to database
     public int create(int empId, String empNm, Date dob, boolean isManager) throws SQLException {
         String sql = "insert into emp_info values(?, ? ,? ,?)";
         PreparedStatement ps = connection.prepareStatement(sql);
@@ -17,9 +15,10 @@ public class DbService {
         ps.setString(2, empNm);
         ps.setDate(3, dob);
         ps.setBoolean(4, isManager );
-        // above lines create SQL statement
+        // above 5 lines create SQL statement
 
         int affected = ps.executeUpdate(); // actually firing the query
+        connection.commit();
         return affected;
     }
 
@@ -27,24 +26,24 @@ public class DbService {
     public int update() {
         return 0;
     }
-
     // delete query
     public int delete() {
         return 0;
     }
-
+    // select query - reading the data from database
     public void find() throws SQLException {
         String sql = "select * from emp_info";
         PreparedStatement ps = connection.prepareStatement(sql);
         ResultSet rs = ps.executeQuery(); // logical representation of physical table
-        while (rs.next()) {
+        while(rs.next()) {
             int id = rs.getInt("emp_id");
             String name = rs.getString("emp_name");
             Date dob = rs.getDate("dob");
             boolean isManager = rs.getBoolean("is_manager");
 
-            System.out.println(" id : " + id + " name : " + name + " dob : " + dob.toString() + " Manager : " + isManager);
+            System.out.println(" id : "+id + " name : " + name +" dob : "+dob.toString() +" Manager : "+isManager);
         }
+        connection.commit();
         rs.close(); // important to close the result set
     }
 
@@ -54,14 +53,34 @@ public class DbService {
         ps.setString(1, name);
 
         ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
+
+        while(rs.next()) {
             int id = rs.getInt("emp_id");
             String nm = rs.getString("emp_name");
             Date dob = rs.getDate("dob");
             boolean isManager = rs.getBoolean("is_manager");
-            System.out.println(" id : " + id + " name : " + nm + " dob : " + dob.toString() + " Manager : " + isManager);
+            System.out.println(" id : "+id + " name : " + nm +" dob : "+dob.toString() +" Manager : "+isManager);
         }
+
         rs.close();
     }
 
+    public void txnDemo(int empId, String name, Date dob, boolean isManager, int upId) throws SQLException {
+        var sql1 = "insert into emp_info values(?, ? , ?, ?)";
+        var ps1 = connection.prepareStatement(sql1);
+        ps1.setInt(1, empId);
+        ps1.setString(2, name);
+        ps1.setDate(3, dob);
+        ps1.setBoolean(4, isManager);
+        var aff1 = ps1.executeUpdate();
+        //connection.commit();
+
+        var sql2 = "update emp_info set emp_name = 'none' where emp_id = ?";
+        var ps2 = connection.prepareStatement(sql2);
+        ps2.setInt(1, upId);
+        var aff2 = ps2.executeUpdate();
+        if(aff2 == 0) connection.rollback();
+
+        connection.commit();
+    }
 }
