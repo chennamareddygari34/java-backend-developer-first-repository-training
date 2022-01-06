467b8a7c661b749f8650a2f71add9c8410c47597
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-06 18:07:20 +0530

    parallel programming future

--- day-3&4/parallel/src/main/java/prll/Main.java
+++ day-3&4/parallel/src/main/java/prll/Main.java
@@ -1,21 +1,17 @@
 package prll;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        main.withoutStream();
-    }
-
-    public void withStream() {
-        IntStream.of(1, 7, 4, 3, 6, 8).parallel().filter(el -> el > 10)
-
-                .forEach(el -> System.out.println(el));
-
-        Stream.of("abc", "pqr", "lmn");
+        main.futures();
     }
 
     public void withoutStream() {
@@ -30,5 +26,39 @@ public class Main {
         for (int num : nums) {
             System.out.println(num * num);
         }
+    }
+
+    public void withStream() {
+        IntStream.of(1, 7, 4, 3, 6, 8).parallel().filter(el -> el > 10).forEach(el -> System.out.println(el));
+        Stream.of("abc", "pqr", "lmn");
+    }
+
+    public void futures() {
+        ExecutorService service = Executors.newFixedThreadPool(5);
+
+        Future<String> task1 = service.submit(() -> {
+            // uploading images to server
+            Thread.sleep(2500);
+            return "Hello World From Task 1";
+        });
+
+        Future<String> task2 = service.submit(() -> {
+            // extracting data from spreadsheet
+            Thread.sleep(5500);
+            return "Hello World From Task 2";
+        });
+
+        try {
+            if (!task1.isCancelled()) {
+                System.out.println(" Task1 Completed : " + task1.get());
+            }
+            if (!task2.isCancelled()) {
+                System.out.println(" Task2 Completed : " + task2.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        service.shutdown();
     }
 }
