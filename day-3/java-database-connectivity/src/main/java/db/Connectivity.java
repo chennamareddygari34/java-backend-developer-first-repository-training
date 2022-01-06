3fdba0d550d02f7e34565db19d0326308e6cad2e
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-06 12:06:42 +0530

    db connectivity : CRUD operations : create

--- day-3/java-database-connectivity/src/main/java/db/Connectivity.java
+++ day-3/java-database-connectivity/src/main/java/db/Connectivity.java
@@ -7,15 +7,20 @@ public class Connectivity {
     private final Connection connection;
 
     public Connectivity() throws ClassNotFoundException, SQLException {
+        // it loads the driver into memory
         Class.forName("com.mysql.cj.jdbc.Driver");
+        // mysql -u root -p
+        // here you are giving username and password for connectivity
         connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/java_backend_training_db",
+                "jdbc:mysql://localhost:3306/java_training_db",
                 "root",
                 "password"
         );
+        System.out.println("---- Connected to MySQL Successfully ----");
     }
 
     public Connection getConnection() {
+
         return connection;
     }
 }
