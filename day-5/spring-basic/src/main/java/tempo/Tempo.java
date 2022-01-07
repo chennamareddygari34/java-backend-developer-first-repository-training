8111334b1c2f0b1444b53f0b0da2ca0d5dd27c7e
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-07 18:01:58 +0530

    spring :ioc container and beans

---
+++ day-5/spring-basic/src/main/java/tempo/Tempo.java
@@ -0,0 +1,13 @@
+package tempo;
+import driver.Driver;
+public class Tempo {
+    private Driver driver;
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
