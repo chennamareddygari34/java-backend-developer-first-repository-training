e891677a57239d7fd7ee87a82254573b4a634649
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-10 14:33:55 +0530

    spring aop:detailed understanding of aspect oriented programming

--- week-2/day-1/src/main/java/com/ani/config/AppConfig.java
+++ week-2/day-1/src/main/java/com/ani/config/AppConfig.java
@@ -1,19 +1,16 @@
 package com.ani.config;
 
-import com.ani.car.Car;
 import com.ani.driver.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy //  hey spring kindly search for all the classes which are attached with @Aspect annotation
 @ComponentScan("com.ani") // hey spring you will find all beans in side given package
 @Configuration
 
 public class AppConfig {
-    @Bean
-    public Car car() {
-        return new Car();
-    }
 
     @Bean
     public Driver driver() {
