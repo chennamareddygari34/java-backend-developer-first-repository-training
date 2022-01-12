be31a7287ec2e67c0200c6b54fc7026d28d84566
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-12 18:19:51 +0530

    template jdbc setup

---
+++ week-2/day-3/spring-data-jdbc/src/test/java/com/ani/springdata/jdbc/SpringDataJdbcApplicationTests.java
@@ -0,0 +1,10 @@
+package com.ani.springdata.jdbc;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+
+class SpringDataJdbcApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
