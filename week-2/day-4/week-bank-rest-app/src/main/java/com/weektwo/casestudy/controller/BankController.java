50fae028b5b42ffc30e0cf40ffcd2d91db3b6de6
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-13 15:29:09 +0530

    logging

--- week-2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/controller/BankController.java
+++ week-2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/controller/BankController.java
@@ -1,30 +1,38 @@
 package com.weektwo.casestudy.controller;
-
 import com.weektwo.casestudy.domain.BankAccount;
 import com.weektwo.casestudy.dto.AppResponse;
 import com.weektwo.casestudy.exception.InvalidAmountException;
 import com.weektwo.casestudy.service.BankService;
+import com.weektwo.casestudy.service.BankServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 @RequestMapping("/bank")
 @RestController // = @Component
 public class BankController {
+
+    private final Logger logger = LoggerFactory.getLogger(BankController.class);
+
     @Autowired
     private BankService service;
+
     @PostMapping // POST -> http://localhost:8080/bank/
     public ResponseEntity<AppResponse<Integer>> createBankAccount(@RequestBody BankAccount ba) {
+
+        logger.info("creating bank account");
+
         service.createNewAccount(ba);
+
         var response = new AppResponse<Integer>();
         response.setMsg("account created successfully");
         response.setSts("success");
         response.setBody(0);
         return ResponseEntity.ok(response);
     }
-
     @PutMapping("/withdraw") // PUT -> http://localhost:8080/123456
     public ResponseEntity<AppResponse<Double>> withdrawMoney(@RequestBody BankAccount ba) {
         try {
@@ -34,7 +42,7 @@ public class BankController {
             response.setSts("success");
             response.setBody(amt);
             return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (InvalidAmountException e) {
+        }catch (InvalidAmountException e) {
             var response = new AppResponse<Double>();
             response.setMsg(e.getMessage());
             response.setSts("fail");
@@ -42,7 +50,6 @@ public class BankController {
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
     }
-
     @PutMapping("/deposit") // PUT -> http://localhost:8080/bank/deposit
     public ResponseEntity<AppResponse<Double>> depositMoney(@RequestBody BankAccount ba) {
         try {
@@ -52,7 +59,7 @@ public class BankController {
             response.setSts("success");
             response.setBody(amt);
             return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
-        } catch (InvalidAmountException e) {
+        }catch (InvalidAmountException e) {
             var response = new AppResponse<Double>();
             response.setMsg(e.getMessage());
             response.setSts("fail");
@@ -60,7 +67,6 @@ public class BankController {
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
     }
-
     @GetMapping("/{prefix}")
     public ResponseEntity<AppResponse<List<BankAccount>>> accountsStartWith(@PathVariable String prefix) {
         var response = new AppResponse<List<BankAccount>>();
