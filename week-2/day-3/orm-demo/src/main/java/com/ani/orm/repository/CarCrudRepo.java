37d9007a1328555d5760474e0ba8cf752fb2c5fb
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-13 10:16:25 +0530

    spring data : crud operations

--- week-2/day-3/orm-demo/src/main/java/com/ani/orm/repository/CarCrudRepo.java
+++ week-2/day-3/orm-demo/src/main/java/com/ani/orm/repository/CarCrudRepo.java
@@ -3,7 +3,10 @@ import com.ani.orm.domain.Car;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
 
 public interface CarCrudRepo extends CrudRepository<Car, Long > {
+    @Override
+    List<Car> findAll();
 }
