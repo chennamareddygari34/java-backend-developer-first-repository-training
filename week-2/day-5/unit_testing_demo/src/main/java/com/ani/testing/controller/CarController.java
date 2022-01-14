a40735be2ab1c8949381bedfcec8f6b3a7ce7cd3
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-14 10:46:27 +0530

    unit testing: rest layer

--- week-2/day-5/unit_testing_demo/src/main/java/com/ani/testing/controller/CarController.java
+++ week-2/day-5/unit_testing_demo/src/main/java/com/ani/testing/controller/CarController.java
@@ -1,14 +1,11 @@
 package com.ani.testing.controller;
 import com.ani.testing.domain.Car;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.time.LocalDate;
-
 @RequestMapping("/car")
 @RestController
 public class CarController {
@@ -20,5 +17,10 @@ public class CarController {
         car.setModel("abc");
         car.setMfg(Date.valueOf(LocalDate.now()));
         return ResponseEntity.ok(car);
+    }
+
+    @PostMapping
+    public ResponseEntity<Car> saveCar(@RequestBody Car car) {
+        return new ResponseEntity<>(car, HttpStatus.CREATED);
     }
 }
