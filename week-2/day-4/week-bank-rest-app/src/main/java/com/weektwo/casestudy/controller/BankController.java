a45efad5bf1ef1325cb87e6c7333b8cda351f444
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-18 23:18:51 +0530

    week2-bank-rest-app

--- week-2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/controller/BankController.java
+++ week-2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/controller/BankController.java
@@ -10,30 +10,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 @RequestMapping("/bank")
 @RestController // = @Component
 public class BankController {
-
-    private final Logger logger = LoggerFactory.getLogger(BankController.class);
-
     @Autowired
     private BankService service;
 
     @PostMapping // POST -> http://localhost:8080/bank/
     public ResponseEntity<AppResponse<Integer>> createBankAccount(@RequestBody BankAccount ba) {
-
-        logger.info("creating bank account");
-
         service.createNewAccount(ba);
-
         var response = new AppResponse<Integer>();
         response.setMsg("account created successfully");
         response.setSts("success");
         response.setBody(0);
         return ResponseEntity.ok(response);
     }
-    @PutMapping("/withdraw") // PUT -> http://localhost:8080/123456
+
+    @PutMapping // PUT -> http://localhost:8080/123456
     public ResponseEntity<AppResponse<Double>> withdrawMoney(@RequestBody BankAccount ba) {
         try {
             double amt = service.withdraw(ba.getAcNum(), ba.getBalance());
@@ -42,7 +38,7 @@ public class BankController {
             response.setSts("success");
             response.setBody(amt);
             return new ResponseEntity<>(response, HttpStatus.OK);
-        }catch (InvalidAmountException e) {
+        } catch (InvalidAmountException e) {
             var response = new AppResponse<Double>();
             response.setMsg(e.getMessage());
             response.setSts("fail");
@@ -67,12 +63,78 @@ public class BankController {
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PutMapping("/withdraw") // PUT -> http://localhost:8080/bank/withdraw
+    public ResponseEntity<AppResponse<Double>> withdraw(@RequestBody BankAccount ba) {
+        try {
+            double amt = service.withdraw(ba.getAcNum(), ba.getBalance());
+            var response = new AppResponse<Double>();
+            response.setMsg("money withdraw successfully");
+            response.setSts("success");
+            response.setBody(amt);
+            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+        } catch (InvalidAmountException e) {
+            var response = new AppResponse<Double>();
+            response.setMsg(e.getMessage());
+            response.setSts("fail");
+            response.setBody(0.0);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
     @GetMapping("/{prefix}")
     public ResponseEntity<AppResponse<List<BankAccount>>> accountsStartWith(@PathVariable String prefix) {
         var response = new AppResponse<List<BankAccount>>();
         response.setMsg("account list");
         response.setSts("success");
         response.setBody(service.namesStartsWith(prefix));
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/accnum")
+    public ResponseEntity<AppResponse<BankAccount>> findAccountByAcNum(@RequestBody BankAccount ba) {
+        BankAccount acNum = service.findAccountByAcNum(ba.getAcNum());
+        var response = new AppResponse<BankAccount>();
+        response.setMsg("account found by AcNum");
+        response.setSts("success");
+        response.setBody(acNum);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+//        }catch (InvalidAmountException e) {
+//            var response = new AppResponse<BankAccount>();
+//            response.setMsg(e.getMessage());
+//            response.setSts("fail");
+//            response.setBody(0);
+//            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+//        }
+    }
+
+    @PutMapping("/activate")
+    public ResponseEntity<AppResponse<Boolean>> activate(@RequestBody BankAccount ba) {
+        Boolean stat = service.activateAccount(ba.getAcNum());
+        var response = new AppResponse<Boolean>();
+        response.setMsg("account activated");
+        response.setSts("success");
+        response.setBody(stat);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/deactivate")
+    public ResponseEntity<AppResponse<Boolean>> deactivate(@RequestBody BankAccount ba) {
+        Boolean stat = service.deActivateAccount(ba.getAcNum());
+        var response = new AppResponse<Boolean>();
+        response.setMsg("account deactivated");
+        response.setSts("success");
+        response.setBody(stat);
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<AppResponse<List<BankAccount>>> findall() {
+        var response = new AppResponse<List<BankAccount>>();
+        response.setMsg("account list");
+        response.setSts("success");
+        response.setBody(service.findAllBankAccounts());
         return ResponseEntity.ok(response);
     }
 }
--- week-2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankServiceImpl.java
+++ week-2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankServiceImpl.java
@@ -17,37 +17,70 @@ import java.util.Optional;
         noRollbackFor = InvalidAmountException.class
 )
 @Service
-public class BankServiceImpl implements BankService{
-
+public class BankServiceImpl implements BankService {
     private final Logger logger = LoggerFactory.getLogger(BankServiceImpl.class);
-
     @Autowired
     private BankRepository repository;
-
     @Override
     public void createNewAccount(BankAccount ba) {
         repository.save(ba);
     }
+
     @Override
     public int updateAccountDetails(BankAccount ba) {
-        return 0;
-    }
-    @Override
-    public boolean activateAccount(Long acNum) {
-        return false;
-    }
-    @Override
-    public boolean deActivateAccount(Long acNum) {
-        return false;
+        return  0;
     }
 
     @Override
-    public double withdraw(Long acNum, double amt) throws InvalidAmountException {
-        logger.info("Withdrawing Money from "+acNum +" with Amount  "+amt);
-        logger.warn("Make sure amount possittive");
-        repository.withdraw(amt, acNum);
-        return amt;
+    public boolean activateAccount(Long acNum) {
+        Optional<BankAccount> op = repository.findById(acNum);
+        BankAccount baOld = op.orElseThrow();
+        boolean existingstatus = baOld.getStatus();
+        boolean newstatus = Boolean.parseBoolean("true");
+        BankAccount baNew = new BankAccount();
+        baNew.setBalance(baOld.getBalance());
+        baNew.setAcCrDt(baOld.getAcCrDt());
+        baNew.setStatus(newstatus);
+        baNew.setAcHldNm(baOld.getAcHldNm());
+        baNew.setAcNum(baOld.getAcNum());
+        repository.save(baNew);
+        return baNew.getStatus();
     }
+
+    @Override
+    public boolean deActivateAccount(Long acNum) {
+        Optional<BankAccount> op = repository.findById(acNum);
+        BankAccount baOld = op.orElseThrow();
+        boolean existingstatus = baOld.getStatus();
+        boolean newstatus = Boolean.parseBoolean("false");
+        BankAccount baNew = new BankAccount();
+        baNew.setBalance(baOld.getBalance());
+        baNew.setAcCrDt(baOld.getAcCrDt());
+        baNew.setStatus(newstatus);
+        baNew.setAcHldNm(baOld.getAcHldNm());
+        baNew.setAcNum(baOld.getAcNum());
+        repository.save(baNew);
+        return baNew.getStatus();
+    }
+
+    @Override
+    public double withdraw(Long acNum, double amt) {
+        if (amt <= 0) throw new InvalidAmountException("Amount Should be Non Zero Positive " + amt);
+        Optional<BankAccount> op = repository.findById(acNum);
+        BankAccount baOld = op.orElseThrow();
+        double existingBalance = baOld.getBalance();
+        double newBalance = existingBalance - amt;
+        BankAccount baNew = new BankAccount();
+        baNew.setBalance(newBalance);
+        baNew.setAcCrDt(baOld.getAcCrDt());
+        baNew.setStatus(baOld.getStatus());
+        baNew.setAcHldNm(baOld.getAcHldNm());
+        baNew.setAcNum(baOld.getAcNum());
+        repository.save(baNew);
+        return baNew.getBalance();
+    }
+
+
     @Override
     public double deposit(Long acNum, double amt) throws InvalidAmountException {
         // just explanation I am using this strategy
@@ -67,20 +100,41 @@ public class BankServiceImpl implements BankService{
 //        withdraw(acNum, 10);
         return baNew.getBalance();
     }
+
     @Override
     public int transferMoney(Long srcAc, Long dstAc, double amt) throws InvalidAmountException {
         return 0;
     }
+
+
     @Override
-    public BankAccount findAccountByAcNum(Long acNum) {
-        return null;
+    public BankAccount findAccountByAcNum(Long acNum) throws InvalidAmountException {
+        Optional<BankAccount> op = repository.findById(acNum);
+        BankAccount baOld = op.orElseThrow();
+//        Long existingacc = baOld.getAcNum();
+//        double newacc = ;
+//        BankAccount baNew = new BankAccount();
+//        baNew.setBalance(baOld.getBalance());
+//        baNew.setAcCrDt(baOld.getAcCrDt());
+//        baNew.setStatus(baOld.getStatus());
+//        baNew.setAcHldNm(baOld.getAcHldNm());
+//        baNew.setAcNum(existingacc);
+        return repository.save(baOld);
+//        return baNew.getAcNum();
     }
+
     @Override
     public List<BankAccount> findAllBankAccounts() {
-        return null;
+        List<BankAccount> op = repository.findAll();
+        return op ;
     }
+
+
+
     @Override
     public List<BankAccount> namesStartsWith(String prefix) {
         return repository.findByAcHldNmStartingWith(prefix);
     }
 }
+
+
