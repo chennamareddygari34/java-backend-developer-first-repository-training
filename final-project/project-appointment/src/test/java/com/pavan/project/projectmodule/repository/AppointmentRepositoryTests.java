5592488d84bc90dc7f52887e0eebf9e0eaabf13c
chennamareddygari.p <chennamareddygari.p@collabera.com>
2022-01-23 18:52:58 +0530

    final-project

---
+++ final-project/project-appointment/src/test/java/com/pavan/project/projectmodule/repository/AppointmentRepositoryTests.java
@@ -0,0 +1,10 @@
+package com.pavan.project.projectmodule.repository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+
+public class AppointmentRepositoryTests {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ final-project/project-appointment/src/test/java/com/pavan/project/projectmodule/services/AppointmentServicesTests.java
@@ -0,0 +1,10 @@
+package com.pavan.project.projectmodule.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class AppointmentServicesTests {
+    @Test
+    void contextLoads() {
+    }
+}
